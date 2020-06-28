d33624b5f57f19f33273fd398649352c96b5cd0d
DevSam30 <37780194+DevSam30@users.noreply.github.com>
2020-06-28 22:41:07 +0300

    First attempt lab 4

--- index.php
+++ index.php
@@ -1,44 +1,50 @@
 <?php
-	include_once 'apiHandler.php';
-	include_once '../../../DBConnector.php';
-
-	$api=new ApiHandler();
-	if ($_SERVER['REQUEST_METHOD']==='POST') {
-		//check api key from the header
-		$api_key_correct=false;
-		$headers=apache_request_headers();
-		$header_api_key=$headers['Authorization'];
-		$api_key_correct=$api->checkApiKey();
-
-		if ($api_key_correct) {
-			//creating an order
-			$name_of_food=$_POST['name_of_food'];
-			$number_of_units=$_POST['number_of_units'];
-			$unit_price=$_POST['unit_price'];
-			$order_status=$_POST['order_status'];
-
-			$con=new DBConnector();
-
-			$api->setMealName($name_of_food);
-			$api->setMealUnits($number_of_units);
-			$api->setUnitPrice($unit_price);
-			$api->setStatus($order_status);
-			$res=$api->createOrder();
-
-			if ($res) {
-				//create json and respond
-				$response_array=['success'=>1,'message'=>'Order has been placed'];
-				header('Content-type: application/json');
-				echo json_encode($response_array);
-			}else{
-				$response_array=['success'=>0,'message'=>'Wrong API key'];
-				header('Content-type: application/json');
-				echo json_encode($response_array);
-			}
-		}else if ($_SERVER['REQUEST_METHOD']==='GET') {
-			//for retriving order status
-		}else{
-			//sorry we are not supporting this for now
-		}
-	}
+	
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Title</title>
+	<script src="jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="placeholder.js"></script>
+
+	<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+</head>
+<body>
+
+	<h3>It is time to communicate with the exposed API, all we need is the API key to be passed in the header</h3>
+	<hr>
+	<h4>Feature 1- Placing an order</h4>
+	<hr>
+	<form name="order_form" id="order_form">
+		<fieldset>
+			<legend>Place order</legend>
+			<input type="text" name="name_of_food" id="name_of_food" required placeholder="name of food"/><br>
+
+			<input type="number" name="number_of_units" id="number_of_units" required placeholder="number of units"/><br>
+
+			<input type="number" name="unit_price" id="unit_price" required placeholder="unit price" value="order placed" /><br><br>
+
+			<button type="submit">Place Order</button>
+
+		</fieldset>
+	</form>
+
+	<hr>
+	<h4>2. Feature 2 - Checking Order Status</h4>
+	<hr>
+	<form name="order_status_form" id="order_status_form" method="post" action="<?=$_SERVER['PHP_SELF']?>">
+		<fieldset>
+			<legend>check order status</legend>
+			<input type="number" name="order_id" id="order_id" required placeholder="order ID"/><br><br>
+
+			<button type="submit">Check order status</button>
+		</fieldset>
+	</form>
+
+</body>
+</html>
